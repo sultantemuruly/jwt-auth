f7baa28e1833462c741f7ebfc25262e920d253db
sultantemuruly <sultantemuruly@gmail.com>
2025-08-28 11:53:16 +0500

    feat: add register api

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = auth()->login($user);
+
+        return response()->json([
+            'message' => 'User successfully registered',
+            'user' => $user,
+            'authorization' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ],
+        ], 201);
+    }
+}
